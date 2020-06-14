1c25b23b29801081b7b3a0dc352e8414ea484b81
valkyrie-max <alisakitkina2016@gmail.com>
2020-06-14 16:37:43 -0500

    made legal text to pop up when legal is clicked; made legal text to close when closing icon is clicked; finished styling the bottom content

---
+++ script.js
@@ -0,0 +1,8 @@
+// open and close the legal text
+const legalText = document.getElementsByClassName(`legalText`); 
+function openText() {
+    legalText[0].style.display = "flex";
+} 
+function closeText() {
+    legalText[0].style.display = "none";
+} 
