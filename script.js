36d7a855e6ec0b79d9933fdc3c365082ba078af8
valkyrie-max <alisakitkina2016@gmail.com>
2020-06-14 19:26:52 -0500

    finished animating the main info

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // open and close the legal text
 const legalText = document.getElementsByClassName(`legalText`); 
-function openText() {
-    legalText[0].style.display = "flex";
+const openText = () => {
+    legalText[0].style.display = `flex`;
 } 
 function closeText() {
-    legalText[0].style.display = "none";
+    legalText[0].style.display = `none`;
 } 
+
+// animations between frames
+const tl = new TimelineMax({delay: 1, repeat: -1, repeatDelay: 0})
+tl.staggerFrom(`.animation`, 1, {x:-50, opacity:0}, 5)
+tl.staggerTo(`.animation`, 1, {zIndex: -30, x:-100, opacity:0}, 5, 5);
